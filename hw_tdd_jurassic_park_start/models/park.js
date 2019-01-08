409fc77af317a96bb55592c0ed02c6d38797a805
klkirstyleeskl <k.lees@btinternet.com>
2019-01-08 20:21:40 +0000

    passes dino remove test

--- hw_tdd_jurassic_park_start/models/park.js
+++ hw_tdd_jurassic_park_start/models/park.js
@@ -1,7 +1,15 @@
-const Park = function (name, ticketPrice, dinoList = []) {
+const Park = function (name, ticketPrice, dinoList) {
   this.name = name;
   this.ticketPrice = ticketPrice;
-  this.dinoList = dinoList;
+  this.dinoList = dinoList = [ ];
 }
+
+Park.prototype.addDino = function (dinosaur) {
+  this.dinoList.push(dinosaur);
+};
+
+Park.prototype.removeDino = function (dinosaur) {
+  this.dinoList.pop(dinosaur);
+};
 
 module.exports = Park;
--- hw_tdd_jurassic_park_start/specs/park_spec.js
+++ hw_tdd_jurassic_park_start/specs/park_spec.js
@@ -7,19 +7,36 @@ describe('Park', function() {
   beforeEach(function () {
     park = new Park('DinoLand', 5.99, [])
   });
+
   it('should have a name', function (){
     const actual = park.name;
     assert.strictEqual(actual, 'DinoLand');
   });
 
 
-  it('should have a ticket price');
+  it('should have a ticket price', function(){
+    const actual = park.ticketPrice;
+      assert.strictEqual(actual, 5.99);
+  });
 
-  it('should have a collection of dinosaurs');
 
-  it('should be able to add a dinosaur to its collection');
+  it('should have a collection of dinosaurs', function(){
+    const actual = park.dinoList;
+    assert.deepStrictEqual(actual, []);
+  });
 
-  it('should be able to remove a dinosaur from its collection');
+  it('should be able to add a dinosaur to its collection', function(){
+    park.addDino('diplodocus');
+    const expected = ['diplodocus'];
+    assert.deepStrictEqual(park.dinoList, expected)
+  });
+
+  it('should be able to remove a dinosaur from its collection', function(){
+    park.removeDino('diplodocus');
+    const expected = [];
+    assert.deepStrictEqual(park.dinoList, expected)
+  });
+
 
   it('should be able to find the dinosaur that attracts the most visitors');
 
